3660a26619bdcd69ec61f598b976472a5220a257
Zer0Nu11 <levo950@gmail.com>
2022-10-08 23:03:56 +0300

    fixes

--- src/model/main.py
+++ src/model/main.py
@@ -14,17 +14,16 @@ def main(query_type, role):
         preprocessing.preprocess()
     if query_type == "digest":
         res = postModel.news(role, 3)
-        print(res)
-        return 0
     elif query_type == "trends":
         res = postModel.trends()
-        print(res)
-        return 0
     elif query_type == "preprocess":
         preprocessing.preprocess()
+        res = "data preprocessed"
     else:
         print("main.py ERROR : unknown query")
         return 1
+    print(res)
+    return 0
 
 if __name__ == "__main__":
     main(sys.argv[1],sys.argv[2])
